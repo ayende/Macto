c8e3aa20bdd91ab29074104ccc27a62d55850e21
Ayende Rahien <ayende@ayende.com>
2011-07-09 22:13:58 +0300

    Fixing serialization

--- HibernatingRhinos.Macto/Models/Note.cs
+++ HibernatingRhinos.Macto/Models/Note.cs
@@ -2,11 +2,9 @@
 {
     public class Note
     {
-        public Officer ReportingOfficer { get; set; }
+        public string ReportingOfficerId { get; set; }
         public string Reason { get; set; }
         public ReportType ReportType { get; set; }
         public string Report { get; set; }
-
-        public bool IsCliffNote { get; set; }
     }
 }
