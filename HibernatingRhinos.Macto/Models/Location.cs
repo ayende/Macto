8b6d0c57a6d701d879b8b1950da66ff59a4cdf30
Ayende Rahien <ayende@ayende.com>
2011-07-09 22:13:04 +0300

    Location should contain all of the inmates in it

--- HibernatingRhinos.Macto/Models/Location.cs
+++ HibernatingRhinos.Macto/Models/Location.cs
@@ -1,9 +1,17 @@
-﻿namespace HibernatingRhinos.Macto.Models
+﻿using System.Collections.Generic;
+
+namespace HibernatingRhinos.Macto.Models
 {
     public class Location
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public int RosterCount { get; set; }
+
+		public ICollection<string> Roster { get; set; }
+
+    	public Location()
+    	{
+			Roster = new List<string>();
+    	}
     }
 }
