e9d07cefe20adc548b257b9ae2e1e4568ee93309
Nabil Shuhaiber <nabil@shuhaiber.com>
2011-07-10 17:15:02 +0100

    Added new commands

---
+++ HibernatingRhinos.Macto/Models/Commands/AddCliffNoteCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HibernatingRhinos.Macto.Models.Commands
+{
+    public class AddCliffNoteCommand : ICommand
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HibernatingRhinos.Macto/Models/Commands/FlagDossierCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HibernatingRhinos.Macto.Models.Commands
+{
+    public class FlagDossierCommand : ICommand
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
