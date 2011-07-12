22f736021c83146d539c5a1201a696a21f7ed0ba
Nabil Shuhaiber <nabil@shuhaiber.com>
2011-07-12 21:55:52 +0100

    Added type for raven attachments

---
+++ HibernatingRhinos.Macto/Models/AttachmentReference.cs
@@ -0,0 +1,12 @@
+﻿namespace HibernatingRhinos.Macto.Models
+{
+    public class AttachmentReference
+    {
+        public string AttachmentId { get; set; }
+
+        public static implicit operator AttachmentReference(string id)
+        {
+            return new AttachmentReference() { AttachmentId = id };
+        }
+    }
+}
